dfff3ade7bfb3df9cae154310187b3e99ce307c5
kibambe81 <kibambe81@github.com>
2026-02-22 21:09:14 +0000

    Configure for Render.com deployment

--- server.ts
+++ server.ts
@@ -321,7 +321,7 @@ if (process.env.NODE_ENV !== "production") {
   });
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
